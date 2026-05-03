ac96da55d9cd56c907b7ca0f54e03935eebcab78
TooryanStudios <master.admin@tooryan.com>
2026-05-03 13:59:33 +0400

    fix: make hostgator deploy script ESM-compatible

--- admin-app/deploy-to-hostgator.js
+++ admin-app/deploy-to-hostgator.js
@@ -1,5 +1,9 @@
-const FtpDeploy = require('ftp-deploy');
-const path = require('path');
+import FtpDeploy from 'ftp-deploy'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const config = {
     user: process.env.HOSTGATOR_FTP_USERNAME,
@@ -11,14 +15,14 @@ const config = {
     include: ['*', '**/*'],
     deleteRemote: false, // Set to true to delete remote files not in local
     forcePasv: true
-};
+}
 
 const ftpDeploy = new FtpDeploy();
 
-ftpDeploy.on('log', data => {
-    console.log(data);
-});
+ftpDeploy.on('log', (data) => {
+    console.log(data)
+})
 
 ftpDeploy.deploy(config)
-    .then(res => console.log('Deployment complete:', res))
-    .catch(err => console.error('Deployment failed:', err));
+    .then((res) => console.log('Deployment complete:', res))
+    .catch((err) => console.error('Deployment failed:', err))
